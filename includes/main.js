816fa29ce71d75c4c29b824b698e9aefa83c8fc0
hadarivan <hadarosh1995@gmail.com>
2018-07-04 10:59:47 +0300

    3 flows

--- includes/main.js
+++ includes/main.js
@@ -3,6 +3,34 @@ $("#process").click(function () {
     alert("הסידור נוצר בהצלחה");
 });
 
+var popup3 = document.getElementById('popup3');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById('check');
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn1.onclick = function() {
+    popup3.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    popup3.style.display = "none";
+};
+
+var save = document.getElementById('button2');
+save.onclick = function () {
+    popup3.style.display = "none";
+    var change=document.getElementById('text')
+    change.innerHTML="שם: הברית של יואב<br>\n" +
+        "                תאריך:18.8.2018<br>\n" +
+        "                סוג מנה:רגילה<br>\n" +
+        "                סטטוס הגעה:בתהליך"
+};
+
 var popup1 = document.getElementById('popup1');
 
 // Get the button that opens the modal
@@ -131,4 +159,6 @@ update.onclick = function () {
     popup2.style.display = "none";
 
 }
+
+
 
